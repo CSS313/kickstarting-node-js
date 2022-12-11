13c5de3fdc3d5891c509cffeaa0dc9391aca3983
CSS313 <singh.chetan.313@gmail.com>
2022-12-11 13:25:10 +0530

    add code to read write operations from file

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
     // console.log("Chetan Singh")
     const url = req.url
+    const method = req.method
+
+    if(url === '/message' && method === 'POST') {
+        const body = []
+        req.on('data', (chunk) => {
+            body.push(chunk)
+        })
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString()
+            const message = parsedBody.split('=')[1]
+            fs.writeFileSync('message.txt', message)
+        })
+        
+        
+        res.statusCode = 302
+        res.setHeader('Location', '/')
+        return res.end()
+    }
+
     if(url === '/home') {
         res.setHeader('Content-Type', 'text/html')
         res.write('<html>')
@@ -28,12 +49,17 @@ const server = http.createServer((req, res) => {
         return res.end()
     }
     else {
-        res.setHeader('Content-Type', 'text/html')
-        res.write('<html>')
-        res.write('<head><title>My First Node js Page</title></head>')
-        res.write('<body><h1>Welcome home</h1></body>')
-        res.write('</html>')
-        return res.end()
+        fs.readFile('message.txt', {encoding: 'utf-8'}, (err, data) => {
+            if(err) { console.log(err) }
+            res.setHeader('Content-Type', 'text/html')
+            res.write('<html>')
+            res.write('<head><title>My First Node js Page</title></head>')
+            res.write(`<body>${data}</body>`)
+            res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+            res.write('</html>')
+            return res.end()
+        })
+        
     }
     
 })
